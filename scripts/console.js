6ad7d7dce5da9909f79b51b18e67e29f56f55e29
Office Of Naval Intelligence <142657201+OFFICEOFNAVALINTELLIGENCE@users.noreply.github.com>
2023-10-08 12:10:33 -0600

    Testing Authcode assignment

--- scripts/console.js
+++ scripts/console.js
@@ -1,64 +1,78 @@
 const validMIDs = {
-    "0256-8964-AA": ["midassignment", "perscom - secret", "section 0 - topsecret", "blacklist - restricted"],
-    "6896-4689-C357": ["midassignment", "perscom - secret", "section 0 - topsecret", "blacklist - restricted"],
-    "0600-9935-KE": ["", "group4"], // Locked to group2 and group4
-    "3071-8108-SR": ["midassignment", "perscom - secret", "blacklist - restricted"],
-    "3251-9753-AH": ["perscom - secret", "blacklist - restricted"],
-    "7964-6447-SS": ["", "blacklist - restricted"],
-    "5896-4673-BB": ["midassignment", "perscom - secret", "blacklist - restricted"],
-    "4790-4673-DR": ["", "blacklist - restricted"],
-    "6888-4674-BG": ["", "blacklist - restricted"],
-    "2586-4896-JS": ["", "blacklist - restricted"]
+    "0256-8964-AA": {
+        groups: ["midassignment", "perscom - secret", "section 0 - topsecret", "blacklist - restricted"],
+        authCode: "November" // Assign an authorization code to the MID
+    },
+    "6896-4689-C357": {
+        groups: ["midassignment", "perscom - secret", "section 0 - topsecret", "blacklist - restricted"],
+        authCode: "authcode2" // Assign an authorization code to the MID
+    },
+    "0600-9935-KE": {
+        groups: ["", "group4"], // Locked to group2 and group4
+        authCode: "authcode3" // Assign an authorization code to the MID
+    },
+    // Add more MIDs and their associated data here
 };
 
 let userMID = "";
 let userGroups = [];
+let userAuthCode = "";
+let authorized = false;
 
-// Hide the group selection initially
 document.getElementById('group-select-container').style.display = 'none';
+document.getElementById('auth-code-input').style.display = 'none';
 
 function checkMID() {
     const mid = document.getElementById('mid').value.trim();
 
     if (validMIDs[mid]) {
         userMID = mid;
-        userGroups = validMIDs[mid];
-        showGroupSelection();
+        userGroups = validMIDs[mid].groups; // Retrieve groups from data
+        userAuthCode = validMIDs[mid].authCode; // Retrieve authCode from data
+        showAuthCodeInput();
     } else {
         document.getElementById('validation-message').textContent = 'Invalid MID. Please try again.';
     }
 }
 
-function showAccessGranted() {
-    document.getElementById('access-message').style.display = 'block';
+function showAuthCodeInput() {
+    document.getElementById('mid-input').style.display = 'none';
+    document.getElementById('auth-code-input').style.display = 'block';
 }
 
-function hideAccessGranted() {
-    document.getElementById('access-message').style.display = 'none';
+function checkAuthCode() {
+    const authCode = document.getElementById('auth-code').value;
+
+    if (authCode === userAuthCode) { // Check against user's assigned authCode
+        authorized = true;
+        showGroupSelection();
+    } else {
+        document.getElementById('validation-message').textContent = 'Invalid Authorization Code. Please try again.';
+    }
 }
 
 function showGroupSelection() {
-    document.getElementById('mid-input').style.display = 'none';
-    const groupSelect = document.getElementById('group-select');
-    groupSelect.innerHTML = ''; // Clear previous options
+    if (authorized) {
+        const groupSelect = document.getElementById('group-select');
+        groupSelect.innerHTML = '';
 
-    userGroups.forEach(group => {
-        const option = document.createElement('option');
-        option.value = group;
-        option.text = group;
-        groupSelect.appendChild(option);
-    });
+        userGroups.forEach(group => {
+            const option = document.createElement('option');
+            option.value = group;
+            option.text = group;
+            groupSelect.appendChild(option);
+        });
 
-    // Show the group selection
-    document.getElementById('group-select-container').style.display = 'block';
+        document.getElementById('group-select-container').style.display = 'block';
+    } else {
+        document.getElementById('validation-message').textContent = 'Access granted. You are not authorized.';
+    }
 }
 
 function redirectBasedOnSelection() {
-    const selectedGroup = document.getElementById('group-select').value;
+    if (authorized) {
+        const selectedGroup = document.getElementById('group-select').value;
 
-    // Check if the user's MID is in the selected group
-    if (userGroups.includes(selectedGroup)) {
-        // Redirect based on the selected group
         switch (selectedGroup) {
             case 'midassignment':
                 window.location.href = 'mid.html';
